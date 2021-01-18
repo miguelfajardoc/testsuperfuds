f6e38f7747d33d5b7f0c3d12cccd879c3f55c153
miguelfajardoc <47123966+miguelfajardoc@users.noreply.github.com>
2021-01-18 14:35:53 -0500

    remove key and pasword from repository

--- superfoodsApi/settings.py
+++ superfoodsApi/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n)f+@kaun9xdukd=&m$42ch&olv)ju3q5-fq-os18(ly-i08ai'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
@@ -84,8 +81,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'test',
         'USER': 'testuser',
-        'PASSWORD': '$up3rFud$',
-        'HOST': 'dev-superfuds.cm8heorrngqo.sa-east-1.rds.amazonaws.com',
+        'PASSWORD': '',
+        'HOST': '',
         'PORT': '3306'
         }
 
